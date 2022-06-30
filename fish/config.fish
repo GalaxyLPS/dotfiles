0cc7fc8ffcf8e9870159b5af0e415270df9e1230
Sören Wicke Iurgel <soeren.wicke.iurgel@gmail.com>
2022-06-30 15:56:49 +0200

    adding current config for some programs
    Signed-off-by: Sören Wicke Iurgel <soeren.wicke.iurgel@gmail.com>

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_add_path ~/.local/bin
+fish_add_path ~/.cargo/bin
+
+source /usr/share/nvm/init-nvm.sh
