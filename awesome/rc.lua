0f4cffaf164035e90c0d1ccb96c3e9ff177b1054
Sören Wicke Iurgel <soeren.wicke.iurgel@gmail.com>
2022-07-04 13:21:31 +0200

    Update awesome init script and use nvchad
    NvChad: https://nvchad.github.io/
    
    Signed-off-by: Sören Wicke Iurgel <soeren.wicke.iurgel@gmail.com>

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -47,7 +47,7 @@ editor_cmd = terminal .. " -e " .. editor
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod1"
+modkey = "Mod4"
 -- }}}
 
 -- {{{ Menu
@@ -235,12 +235,12 @@ awful.keyboard.append_global_keybindings({
         { description = "lua execute prompt", group = "awesome" }),
     awful.key({ modkey, }, "Return", function() awful.spawn(terminal) end,
         { description = "open a terminal", group = "launcher" }),
-    awful.key({ modkey }, "r", function() 
+    awful.key({ modkey, }, "space", function() 
     	awful.spawn("rofi -show run") end,
         { description = "Launch rofi to run a command", group = "launcher" }),
-    awful.key({ modkey }, "space", function()
+    --[[ awful.key({ modkey }, "space", function()
     	awful.spawn("rofi -show window") end,
-        { description = "Launch rofi to start a program", group = "launcher" }),
+        { description = "Launch rofi to start a program", group = "launcher" }), ]]--
 })
 
 -- Tags related keybindings
@@ -570,7 +570,9 @@ awful.keyboard.append_client_keybindings({
 	awful.spawn.with_shell("shutter -s")
 end)})
 
+awful.spawn.with_shell("xrandr --output DP-0 --auto --output HDMI-0 --auto --right-of DP-0")
 awful.spawn.with_shell("nitrogen --head=1 --random --set-scaled ~/Wallpapers")
+awful.spawn.with_shell("nitrogen --head=0 --random --set-scaled ~/Wallpapers")
 awful.spawn.with_shell("picom")
 
 beautiful.gap_single_client = true
